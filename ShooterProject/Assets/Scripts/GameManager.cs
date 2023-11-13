bd9175296c29ab23598bebc594aabb88bb7a453a
lo968854 <lo968854@ucf.edu>
2023-11-12 23:36:15 -0500

    Task 2 + Build
    Task 2 implemented and build created

---
+++ ShooterProject/Assets/Scripts/GameManager.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class GameManager : MonoBehaviour
+{
+
+    public GameObject playerPrefab;
+    public GameObject enemyOnePrefab;
+    public GameObject cloudPrefab;
+    public GameObject coinPrefab;
+    public int score;
+    public int lives;
+    public int cloudsMove;
+    public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI livesText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(playerPrefab, transform.position, Quaternion.identity);
+        CreateSky();
+        InvokeRepeating("SpawnEnemyOne", 1f, 2f);
+        InvokeRepeating("SpawnCoin", 3f, 4f);
+        cloudsMove = 1;
+        score = 0;
+        lives = 3;
+        scoreText.text = "Score: " + score;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        livesText.text = "Lives: " + lives;
+    }
+
+    void SpawnEnemyOne()
+    {
+        Instantiate(enemyOnePrefab, new Vector3(Random.Range(-8, 8), 7.5f, 0), Quaternion.Euler(0, 0, 180));
+    }
+
+    void SpawnCoin()
+    {
+        Instantiate(coinPrefab, new Vector3(-8f, Random.Range(-7f, 4f), 0), Quaternion.identity);
+    }
+
+    void CreateSky()
+    {
+        for (int i = 0; i < 50; i++)
+        {
+            Instantiate(cloudPrefab, new Vector3(Random.Range(-11f, 11f), Random.Range(-7.5f, 7.5f), 0), Quaternion.identity);
+        }
+    }
+
+    public void GameOver()
+    {
+        CancelInvoke();
+        cloudsMove = 0;
+    }
+
+    public void EarnScore(int scoreToAdd)
+    {
+        score = score + scoreToAdd;
+        scoreText.text = "Score: " + score;
+    }
+
+   public void LifeLoss()
+   {
+       lives = lives - 1;
+       //livesText.text = "Lives: " + lives;
+   }
+}
